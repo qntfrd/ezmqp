321c14bbbb95a17df18d77a8ce84d473588a76e8
Quentin Forand <quentin@forand.fr>
2021-05-22 12:13:02 +0200

    feat(connection): validate the connection parameters

--- README.md
+++ README.md
@@ -62,8 +62,6 @@ queue.sub(msg => console.log(msg))
 await queue.cancel()
 ```
 
-## Ack/Nack
-
 Using ezmqp, messages will automatically be acknowledgeed if your handler
 function resolves (and nack if it rejects).
 
@@ -71,3 +69,87 @@ If you want to manually ack/nack, your handler will receive an `ack` function as
 a parameter.
 If you call it without parameters (or a truthy one), it will ack.  
 If you pass false or error, it will nack
+
+
+## Api
+
+- [ezmqp](#ezmqp)
+  - [Usage](#usage)
+  - [Api](#api)
+    - [Rabbit](#rabbit)
+      - [Constructor](#constructor)
+      - [nodes: List server's connection options](#nodes-list-servers-connection-options)
+    - [Types](#types)
+      - [Connection](#connection)
+      - [Configuration](#configuration)
+
+### Rabbit
+
+#### Constructor
+
+```ts
+Rabbit#Constructor(configuration: Configuration) => Rabbit
+Rabbit#Constructor(
+  connection?: string|Connection|Array<string|Connection>,
+  configuration?: Configuration
+) => Rabbit
+```
+
+You can use the constructor either by passing a [Configuration](#configuration)
+object which defines everything for you beforehand, including the connection.
+
+You can also pass in the [Configuration](#configuration) separately from
+connection options by providing either a [Connection](#connection) object or a
+connection string BEFORE the [Configuration](#configuration) object (which then
+becomes optional). In that case, the [Configuration](#configuration) connection
+field will be **ignored** to the profit of what is passed in the connection
+parameter.
+
+If you use a rabbit cluster, you can pass an array of connection string or
+[Connection](#connection) objects. If your connection string has comma `,`, it
+will be considered as a connection to a cluster and the string will be split.
+
+The connection string can have query parameters for frameMax, heartbeat and
+channelMax (e.g. `amqp://localhost/?frameMax=4096`)
+
+#### nodes: List server's connection options
+
+```ts
+get nodes() => Array<Required<Connection>>
+```
+
+Returns the list of connection objects.
+
+Note: Each item can be safely stringified or jsonified because the password will
+be hidden by `****`
+
+### Types
+
+#### Connection
+
+Defines how the connection to a rabbit server must be established
+
+- `protocol: "amqp"|"amqps" = "amqp"` Whether to use amqp or amqps
+- `hostname: string = "localhost"` The rabbit server hostname
+- `port: number = 5672` The port the server listens
+- `username: string = "guest"` The user to connect to the server with
+- `password: string = "guest"` The user's password
+- `locale: "en_US" = "en_US"` The locale to use for rabbit errors (only en_US is
+handeled by rabbit)
+- `frameMax: number = 0` The size in bytes of the maxium amount of data in a
+message. 0 means no limit (which is `2^32-1`)
+- `channelMax: number = 0` The maximum number of channels. 0 means no limit
+(which is `2^16-1`)
+- `heartbeat: number = 0` The frequency in second to check whether the
+connection to the server is up. 0 means takes the server configuration.
+
+Note: For your convienence, the configuration object is partial (which means all
+its fields are optional), however, if you read from it, all its fields will be
+set to their default values.
+
+Also, converting this object as a string will output the connection string, with
+all fields except the default query parameter, and the password hidden by `****`.
+If you try to jsonify the object, the password will also be hidden by `****`.
+
+
+#### Configuration
--- src/rabbit.ts
+++ src/rabbit.ts
@@ -2,9 +2,87 @@ import amqp from "amqplib"
 import { Channel } from "./channel"
 import { Exchange } from "./exchange"
 import { Queue } from "./queue"
-import { Config, ExchangeConfig, QueueConfig } from "./types"
+import { Connection, Configuration, ExchangeConfig, QueueConfig } from "./types"
+
+const connectionStringToObject = (connectionString: string): Required<Connection> => {
+  const url = new URL(connectionString)
+  const params = Object.fromEntries(url.searchParams.entries())
+
+  return validateConnection({
+    protocol: url.protocol.slice(0, -1) as "amqp"|"amqps",
+    hostname: !url.hostname ? "localhost" : url.hostname,
+    username: !url.username ? "guest" : url.username,
+    password: !url.password ? "guest" : url.password,
+    port: !url.port ? 5672 : Number(url.port),
+    // `as any as number` is to pass down any string to the validation to have nice errors
+    channelMax: params.channelMax as any as number ?? 0,
+    frameMax: params.frameMax as any as number ?? 0,
+    heartbeat: params.heartbeat as any as number ?? 0,
+    locale: "en_US",
+    vhost: !url.pathname ? "/" : url.pathname
+  })
+}
+
+/** Validates a connection string and transforms it into a VALID connection
+ *  object
+ *
+ *  @param connectionString - The connection string to parse
+ *  @returns - An array of valid connection object
+ */
+const parseConnectionString = (connectionString: string = "amqp://localhost"): Array<Required<Connection>> => {
+  if (connectionString.includes(","))
+    return connectionString.split(",").map(connectionStringToObject)
+  return [connectionStringToObject(connectionString)]
+}
+
+const validateNumber = (
+  value: string|number|null|undefined,
+  fallback: number = 0,
+  min = 0,
+  max = Number.MAX_SAFE_INTEGER,
+  err = "Invalid number '%s'."
+): number => {
+  if (value === undefined || value === null || value === "") return fallback
+  if (Number.isNaN(Number(value)) || Number(value) != value)
+    throw new Error(err.replace("%s", value + ""))
+  const v = Number(value)
+  if (v < min || v > max)
+    throw new Error(err.replace("%s", value + ""))
+  return v
+}
+
+const validateConnection = (connection: Partial<Connection>): Required<Connection> => {
+
+  const co: Required<Connection> = {
+    protocol: ((): "amqp"|"amqps" => {
+      if  (!connection.protocol) return "amqp"
+      if (connection.protocol === "amqps") return "amqps"
+      if (connection.protocol === "amqp") return "amqp"
+      throw new Error(`Invalid protocol '${connection.protocol}'`)
+    })(),
+
+    hostname: connection.hostname ?? "localhost",
+    username: !!connection.username ? connection.username : "guest",
+    password: !!connection.username ? connection.password : "guest",
+
+    port: validateNumber(connection.port, 5672, 0, 65535, "Invalid port '%s'"),
+    channelMax: validateNumber(connection.channelMax, 0, 0, 2**16-1, "Invalid channelMax '%s'. Expected range between 0 and 2^16-1"),
+    frameMax: validateNumber(connection.frameMax, 0, 0, 2**32-1, "Invalid frameMax '%s'. Expected range between 0 and 2^16-1"),
+    heartbeat: validateNumber(connection.heartbeat, 0, 0, 2**32-1, "Invalid heartbeat '%s'. Expected range between 0 and 2^16-1"),
+
+    locale: connection.locale ?? "en_US",
+    vhost: !!connection.vhost ? connection.vhost : "/"
+  }
+
+  if (co.vhost[0] !== "/") throw new Error(`Invalid vhost '${co.vhost}'. Must start with '/'`)
+
+  return co
+}
 
 export class Rabbit {
+  /** The list of available servers */
+  private servers: Array<Required<Connection>>
+
   /** The actual amqp connection */
   private client: amqp.Connection | null = null
   /** List of existing exchanges */
@@ -15,62 +93,122 @@ export class Rabbit {
   private queues: Map<string, Queue> = new Map()
 
   /** Creates a client by loading a configuration */
-  constructor(private configuration: Config) {}
+  constructor(configuration: Configuration)
+  constructor(connection?: Connection|string|Array<Connection|string>, configuration?: Configuration)
+  constructor(connection?: Connection|Configuration|string|Array<Connection|string>, configuration?: Configuration) {
+    if (connection === null || connection === undefined || typeof connection === "string") {
+      this.servers = parseConnectionString(connection as string)
+    }
+    else if (Array.isArray(connection)) {
+      this.servers = connection.map(co => {
+        if (typeof co === "string") return parseConnectionString(co)
+        else return validateConnection(co)
+      }).flat()
+    }
+    else if (typeof connection === "object" && (
+      connection.hasOwnProperty("protocol")
+      || connection.hasOwnProperty("hostname")
+      || connection.hasOwnProperty("username")
+      || connection.hasOwnProperty("password")
+      || connection.hasOwnProperty("port")
+      || connection.hasOwnProperty("channelMax")
+      || connection.hasOwnProperty("frameMax")
+      || connection.hasOwnProperty("heartbeat")
+      || connection.hasOwnProperty("vhost")
+    )) {
+      this.servers = [validateConnection(connection as Connection)]
+    }
+    else if (connection.hasOwnProperty("connection")) {
+      const co = connection as Configuration
+      if (co.connection === null || co.connection === undefined || typeof co.connection === "string")
+        this.servers = parseConnectionString(co.connection as string)
+      else if (Array.isArray(co.connection)) {
+        this.servers = co.connection.map(c => {
+          if (typeof c === "string") return parseConnectionString(c)
+          else return validateConnection(c)
+        }).flat()
+      }
+      else if (typeof co.connection === "object") {
+        this.servers = [validateConnection(co.connection as Connection)]
+      }
+    }
+  }
+
+  get nodes(): Array<Required<Connection>> {
+    return [...this.servers.map(node => {
+      return {
+        ...node,
+        toString: function() {
+          const cs = `${this.protocol}://${this.username}:****@${this.hostname}:${this.port}${this.vhost}`
+          if (this.heartbeat === 0 && this.channelMax === 0 && this.frameMax === 0)
+            return cs
+          const query = []
+          if (this.frameMax !== 0) query.push(`frameMax=${this.frameMax}`)
+          if (this.channelMax !== 0) query.push(`channelMax=${this.channelMax}`)
+          if (this.heartbeat !== 0) query.push(`heartbeat=${this.heartbeat}`)
+          return `${cs}?${query.join("&")}`
+        },
+        toJSON: function() {
+          return { ...this, password: "****" }
+        }
+      }
+    })]
+  }
 
   /** Enfore the configuartion on the server
    *  that is, create queues and exchange and bind the two together
    */
-  private async setup() {
-    for (const exchange in this.configuration.exchanges) {
-      const cnf = this.configuration.exchanges[exchange]
-      const ex = await this.exchange(cnf.name || exchange, cnf).assert()
-      if (cnf.hasOwnProperty("topics")) {
-        const c = cnf as { topics: {[key: string]: string|string[]}}
-        await Promise.all(Object.keys(c.topics)
-          .map(queue => {
-            const qcnf = this.configuration.queues && this.configuration.queues[queue] || {}
-            const qname = qcnf.name || queue
-            return this.queue(qname, qcnf).assert().then(async q => {
-              if (Array.isArray(c.topics[queue]))
-                return Promise.all((c.topics[queue] as string[]).map(rk => ex.bind(q, rk)))
-              return ex.bind(q, c.topics[queue] as string)
-            })
-          }))
-      }
-      else if (cnf.hasOwnProperty("direct")) {
-        const c = cnf as {direct: {[key: string]: string|string[]}}
-        await Promise.all(Object.keys(c.direct)
-          .map(queue => {
-            const qcnf = this.configuration.queues && this.configuration.queues[queue] || {}
-            const qname = qcnf.name || queue
-            return this.queue(qname, qcnf).assert().then(async q => {
-              if (Array.isArray(c.direct[queue]))
-                return Promise.all((c.direct[queue] as string[]).map(rk => ex.bind(q, rk)))
-              return ex.bind(q, c.direct[queue] as string)
-            })
-          }))
-      }
-      else if (cnf.hasOwnProperty("fanout")) {
-        const c = cnf as { fanout: string[] }
-        await Promise.all(c.fanout.map(queue => {
-          const qcnf = this.configuration.queues && this.configuration.queues[queue] || {}
-          const qname = qcnf.name || queue
-          return this.queue(qname, qcnf).assert().then(q => ex.bind(q))
-        }))
-      }
-    }
-    for (const queue in this.configuration.queues) {
-      const cnf = this.configuration.queues[queue]
-      await this.queue(cnf.name || queue, cnf).assert()
-    }
-  }
+  // private async setup() {
+  //   for (const exchange in this.configuration.exchanges) {
+  //     const cnf = this.configuration.exchanges[exchange]
+  //     const ex = await this.exchange(cnf.name || exchange, cnf).assert()
+  //     if (cnf.hasOwnProperty("topics")) {
+  //       const c = cnf as { topics: {[key: string]: string|string[]}}
+  //       await Promise.all(Object.keys(c.topics)
+  //         .map(queue => {
+  //           const qcnf = this.configuration.queues && this.configuration.queues[queue] || {}
+  //           const qname = qcnf.name || queue
+  //           return this.queue(qname, qcnf).assert().then(async q => {
+  //             if (Array.isArray(c.topics[queue]))
+  //               return Promise.all((c.topics[queue] as string[]).map(rk => ex.bind(q, rk)))
+  //             return ex.bind(q, c.topics[queue] as string)
+  //           })
+  //         }))
+  //     }
+  //     else if (cnf.hasOwnProperty("direct")) {
+  //       const c = cnf as {direct: {[key: string]: string|string[]}}
+  //       await Promise.all(Object.keys(c.direct)
+  //         .map(queue => {
+  //           const qcnf = this.configuration.queues && this.configuration.queues[queue] || {}
+  //           const qname = qcnf.name || queue
+  //           return this.queue(qname, qcnf).assert().then(async q => {
+  //             if (Array.isArray(c.direct[queue]))
+  //               return Promise.all((c.direct[queue] as string[]).map(rk => ex.bind(q, rk)))
+  //             return ex.bind(q, c.direct[queue] as string)
+  //           })
+  //         }))
+  //     }
+  //     else if (cnf.hasOwnProperty("fanout")) {
+  //       const c = cnf as { fanout: string[] }
+  //       await Promise.all(c.fanout.map(queue => {
+  //         const qcnf = this.configuration.queues && this.configuration.queues[queue] || {}
+  //         const qname = qcnf.name || queue
+  //         return this.queue(qname, qcnf).assert().then(q => ex.bind(q))
+  //       }))
+  //     }
+  //   }
+  //   for (const queue in this.configuration.queues) {
+  //     const cnf = this.configuration.queues[queue]
+  //     await this.queue(cnf.name || queue, cnf).assert()
+  //   }
+  // }
 
   /** Connects to the server and apply the configuration */ // TODO: connection recovery and cluster mode
   async connect(): Promise<Rabbit> {
     if (this.client) return this
-    this.client = await amqp.connect(this.configuration.connection)
+    // this.client = await amqp.connect(this.configuration.connection)
 
-    await this.setup()
+    // await this.setup()
 
     return this
   }
